2b56276ad595186e8c5c1611dfc867e3f9d8ad77
theilrig <98066698+theilrig@users.noreply.github.com>
2025-02-11 14:49:57 -0800

    Add files via upload

--- import time.py
+++ import time.py
@@ -1,7 +1,7 @@
 
 from datetime import datetime
 clock = datetime.now()
-ans = 12*2
+ans = 24*2
 ans += 24 - clock.hour 
 ans -= 1 #Check's back agaisn the fact we're continuing this hour 
 print(ans)
